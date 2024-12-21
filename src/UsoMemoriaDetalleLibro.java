31666fab3ab473a7f87e34ce810e1f42d959c0e4
Arturo Valdez <santiguardado27@gmail.com>
2024-12-21 11:47:19 -0600

    Uso de memoria Stack y hit ejemplo detalle de libro

---
+++ src/UsoMemoriaDetalleLibro.java
@@ -0,0 +1,19 @@
+public class UsoMemoriaDetalleLibro {
+    public static void main(String[] args) {
+        //detalle de un libro
+        String tituloLibro = "El señor de los anillos";
+        int yearPublicacion = 1999;
+        boolean libroDisponible = true;
+        double precio = 500.50;
+
+
+        System.out.println(tituloLibro);
+        System.out.println(yearPublicacion);
+        System.out.println(libroDisponible);
+        System.out.println(precio);
+
+        //Modificar titulo libro
+        tituloLibro = "El señor de los Anillos";
+        System.out.println(tituloLibro);
+    }
+}
