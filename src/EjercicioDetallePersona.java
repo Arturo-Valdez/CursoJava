9c173a0b76971777227ff9a144355ebbf168356f
Arturo Valdez <santiguardado27@gmail.com>
2024-12-21 11:54:39 -0600

    Ejercicio detalle persona

---
+++ src/EjercicioDetallePersona.java
@@ -0,0 +1,16 @@
+public class EjercicioDetallePersona {
+    public static void main(String[] args) {
+        String nombreCompleto = "Santiago Valdez";
+        int edad = 25;
+        double altura = 1.75;
+        String paisOrigen = "Mexico";
+        char estadoCivil = 'C'; // C - Casado / S - Soltero
+
+        System.out.println("Hola mi nombre es: " + nombreCompleto);
+        System.out.println("Hola mi edad es: " + edad);
+        System.out.println("Hola mi altura es: " + altura);
+        System.out.println("Hola mi Pais Origen es: " + paisOrigen);
+        System.out.println("Hola mi estado civil es: " + estadoCivil);
+
+    }
+}
