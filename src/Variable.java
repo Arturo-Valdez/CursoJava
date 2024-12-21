07de3e505bca6f0d5313fe53066ed5df26d53156
Arturo Valdez <santiguardado27@gmail.com>
2024-12-21 11:27:04 -0600

    Tipos de Variables

---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        //Variables
+        int edad = 31;
+        double precio = 411.21;
+        boolean disponible = true; // true / false
+        char genero;
+        genero = 'M'; //M - Masculino / F - Femenino
+
+        //Acceder a variables
+        System.out.println(edad);
+        //Modificar valor
+        edad = 55;
+        System.out.println(edad);
+
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+
+}
