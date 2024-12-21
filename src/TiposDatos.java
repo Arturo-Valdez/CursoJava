5509eda2f44e4b27f72b4ad50fa86e3bef86cb2f
Arturo Valdez <santiguardado27@gmail.com>
2024-12-21 12:05:42 -0600

    Tipo de datos

---
+++ src/TiposDatos.java
@@ -0,0 +1,20 @@
+public class TiposDatos {
+    public static void main(String[] args){
+        //Tipos de datos en Java
+        //Enteros
+        byte tipoByte = 127;
+        short tipoShort = 32000;
+        int tipoInt = 2147483647;
+        long tipoLong = 987654321;
+        float tipoFloat = 3.14F;
+        double tioDouble = 3.1315;
+
+        System.out.println("tipoByte = " + tipoByte);
+        System.out.println("tipoShort = " + tipoShort);
+        System.out.println("tipoInt = " + tipoInt);
+        System.out.println("tipoLong = " + tipoLong);
+        System.out.println("tipoFloat = " + tipoFloat);
+
+
+    }
+}
