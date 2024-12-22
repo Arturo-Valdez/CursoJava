e8b59251b5f6d3e7c65d592ceee96f7da6c00044
Arturo Valdez <santiguardado27@gmail.com>
2024-12-21 19:43:13 -0600

    Ejercicio tienda en linea

---
+++ src/EjercicioTiendaEnLinea.java
@@ -0,0 +1,13 @@
+public class EjercicioTiendaEnLinea {
+    public static void main(String[] args){
+        String nombreProducto = "PC Gamer";
+        double precio = 17450.30;
+        double cantidadDisponible = 1002433.33;
+        boolean estaDisponible = true;
+
+        System.out.println("nombreProducto: " + nombreProducto);
+        System.out.println("precio: " + precio);
+        System.out.println("cantidadDisponible: " + cantidadDisponible);
+        System.out.println("estaDisponible: " + estaDisponible);
+    }
+}
